010593059643018fdcfb6384fb156c32489ff7b6
nimagalunaziya <nimagalunaziya@gmail.com>
2024-03-17 12:26:18 +0530

    Test commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import { useState } from "react";
+
+export default function App() {
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [fullname, setFullname] = useState("");
+  const [showTooltip, setShowTooltip] = useState(false);
+
+  const handle = (e) => {
+    e.preventDefault();
+    if (firstname && lastname !== "") {
+      setFullname(firstname + " " + lastname);
+      setShowTooltip(false); // Clear error if both fields are filled
+    } else {
+      setFullname(" ");
+      setShowTooltip(true);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <h1>Full Name Display</h1>
+      <h3>
+        First Name:{" "}
+        <input
+          vlaue={firstname}
+          onChange={(e) => setFirstname(e.target.value)}
+        />
+      </h3>
+      <h3>
+        Last Name:{" "}
+        <input value={lastname} onChange={(e) => setLastname(e.target.value)} />
+      </h3>
+      <form onSubmit={handle}>
+        <button type="submit">Submit</button>
+      </form>
+      <h3> full name: {fullname}</h3>
+      {showTooltip && (
+        <div
+          style={{
+            position: "absolute",
+            backgroundColor: "white",
+            color: "black",
+            padding: "5px 10px",
+            borderRadius: "5px",
+            borderColor: "black",
+            borderStyle: "solid",
+            borderWidth: "2px",
+            fontSize: "15px",
+            top: "103px",
+            left: "29%",
+            transform: "translateX(-50%)",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Please fill out this field
+        </div>
+      )}
     </div>
   );
 }
-
-export default App;
